b01ad2bf7258fd103ac4601c9b88cf88e8bd25e3
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2022-12-04 01:09:36 +0300

    Add files via upload

---
+++ практическая9/proga9-1.py
@@ -0,0 +1,38 @@
+def Zadanie91(n):
+    s = 0
+    a = []
+    for i in range(n):
+        b = []
+        for j in range(n):
+            print('Введите [',i,',',j,'] элемент')
+            b.append(int(input()))
+        a.append(b)
+    print('Исходный массив:')
+    for i in range(n):
+        for j in range(n):
+            print(a[i][j], end=' ')
+        print()
+    for i in range(n):
+        s += a[0][i]
+    ss = s
+    for i in range(n):
+        w = i-1
+        if ss != s:
+            break
+        ss = 0
+        for j in range(n):
+            t = j-1
+            ss += a[w][t]
+    sss = s
+    for i in range(n-1):
+        w = i-1
+        if sss != s:
+            break
+        sss = 0
+        for j in range(n):
+            t = j-1
+            sss += a[t][w]
+    if (sss==ss) and (ss==s) and (s==sss):
+        print("Матрица является магическим квадратом")
+    else:
+        print("Матрица не является магическим квадратом")
---
+++ практическая9/proga9-2.py
@@ -0,0 +1,20 @@
+def Zadanie92(n):
+    A = []
+    for i in range(n):
+        b = []
+        for j in range(n):
+            print('Введите [',i,',',j,'] элемент')
+            b.append(int(input()))
+        A.append(b)
+    print('Исходная матрица:')
+    for i in range(n):
+        for j in range(n):
+            print(A[i][j], end=' ')
+        print()
+    for i in range(n):
+        A[i][0], A[i][n-1] = A[i][n-1], A[i][0]
+    print("Полученная матрица")
+    for i in range(n):
+        for j in range(n):
+            print(A[i][j], end=' ')
+        print()
