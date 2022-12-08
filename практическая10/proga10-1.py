f7e88b5ad9c3fd601a5bce3e6d3c7cd2b020dfaf
Evgenia Leonova <114184123+LeonovaEA@users.noreply.github.com>
2022-12-08 15:20:17 +0300

    Update proga10-1.py

--- практическая10/proga10-1.py
+++ практическая10/proga10-1.py
@@ -1,51 +1,49 @@
-file = open(r'C:/Users/Professional/Downloads/vvod1.txt', 'r')
-filee = open(r'C:/Users/Professional/Downloads/vivod1.txt', 'w+', encoding='UTF-8')
+with open(r'C:/Users/Professional/Downloads/vvod1.txt', 'r') as f:
+    line = f.readlines()
+    l = [element.replace ("\n", "").split() for element in line]
+B = len(l)
+A = len(l[0])
+a = l
+for i in range(B):
+    for k in range(A):
+        a[i][k] = int(a[i][k])
+    print()
 
-A1 = file.readline(1)
-A2 = file.readline(1)
-A3 = file.readline(1)
-A4 = file.readline(1)
-A5 = file.readline(1)
-A6 = file.readline(1)
-A7 = file.readline(1)
-A8 = file.readline(1)
-A9 = file.readline(1)
+def zadanie101(mas):
+    K = B
+    Y = int(input(f'Выберите номер строки из {K} '))
+    Y -= 1
+    b=range(n)
+    for j in b:
+        mas[Y][j]/=mas[Y][Y]
+    return mas
+with open('C:/Users/Professional/Downloads/vivod1.txt', 'w') as f2:
+    f2.write(str(pr(a)))
+    f2.write('\n')
+with open(r'C:/Users/Professional/Downloads/vvod1.txt', 'r')as f:
+    line = f.readlines()
+    l = [element.replace ("\n", "").split() for element in line]
+B = len(l)
+A = len(l[0])
+a = l
+for i in range(B):
+    for k in range(m):
+        a[i][Y] = int(a[i][Y])
+    print()
+def zadanie102(matr):
+    res=[]
+    for j in range(A):
+        tmp=[]
+        for i in range(B):
+            tmp=tmp+[matr[i][j]]
+        res=res+[tmp]
+    return res
 
-n1 = [int(x) for x in A1]
-n2 = [int(x) for x in A2]
-n3 = [int(x) for x in A3]
-n4 = [int(x) for x in A4]
-n5 = [int(x) for x in A5]
-n6 = [int(x) for x in A6]
-n7 = [int(x) for x in A7]
-n8 = [int(x) for x in A8]
-n9 = [int(x) for x in A9]
-
-file.close()
-file = open(r'C:/Users/Professional/Downloads/vvod1.txt', 'r')
-
-R = file.readlines()
-
-e1 = [n1,n2,n3]
-e2 = [n4,n5,n6]
-e3 = [n7,n8,n9]
-
-print(e1)
-print(e2)
-print(e3)
-
-str1 = n1+n2+n3
-str2 = n4+n5+n6
-str3 = n7+n8+n9
-
-st1 = n1+n4+n7
-st2 = n2+n5+n8
-st3 = n3+n6+n9
-
-d1 = n1+n5+n9
-d2 = n7+n5+n3
-
-if (str1 == str2) and (str3 == str1) and (str2==str3) and (str1==st1) and (st1==st2) and (st3==st2) and (st3==d1) and (d1==d2):
+with open('C:/Users/Professional/Downloads/vivod1.txt', 'a+') as f2:
+    f2.write('\n')
+    f2.write(str(transponse(a)))
+    f2.write('\n')
+if open('C:/Users/Professional/Downloads/vivod1.txt', 'a+'): 
     print(" Матрица явяется магическим квадратом")
     filee.write('\n')
     filee.write(str("Матрица явяется магическим квадратом"))
@@ -56,3 +54,4 @@ else:
     filee.write('\n')
     filee.write(str("Матрица не явяется магическим квадратом"))
     filee.close()
+
